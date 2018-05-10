17038e3c16ec322d354bfb4b8ca4beecf8997008
Glitch (lumpy-windshield-1) <none>
2018-05-10 05:31:40 +0000

    👬🌯 Checkpoint ./bot.js:1028656/986

--- bot.js
+++ bot.js
@@ -1,27 +1,31 @@
 let twit = require('twit');
 
 var T = new twit({
-  consumer_key:         '',
-  consumer_secret:      '',
-  access_token:         '',
-  access_token_secret:  '',
+  consumer_key:         'OiWMuaMhAeJiWxNjHCJQ',
+  consumer_secret:      '0A7oF9Tm3MyUNh5u1I9QZgigPcOPEcZwa7a36LLUY',
+  access_token:         '83818420-e1YGrC5O7VZyrBerSF8KWi0GfTEkN8eM8X69i6VQ',
+  access_token_secret:  'v5saVSMabFPFQFMIylJrE2C0mNpdG3fZyjKKssaIjHA',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 })
 
-// T.post('statuses/update', { status: 'hallo i\'m Bot' }, function(err, data, response) {
-//   console.log(data)
-// })
-function twiting(){
-    var number = Math.floor(Math.random()*100)
 
-    var status = {
-      status : 'hallo i\'m Robot, and this is a random number '+ number 
-    }
-   return T.post('statuses/update', status , function(err, data, response) {
-    // console.log(data)
-    console.log('sukses update twit')
-  })
-}
+var stream = T.stream('statuses/filter', { track: 'balinux' })
+ 
+stream.on('tweet', function (tweet) {
+  console.log(tweet)
+})
 
-// twiting();
-setInterval(twiting, 1000*20);
+// function twiting(){
+//     var number = Math.floor(Math.random()*100)
+
+//     var status = {
+//       status : 'hallo i\'m Robot, and this is a random number '+ number 
+//     }
+//    return T.post('statuses/update', status , function(err, data, response) {
+//     // console.log(data)
+//     console.log('sukses update twit')
+//   })
+// }
+
+// // twiting();
+// setInterval(twiting, 1000*20);
